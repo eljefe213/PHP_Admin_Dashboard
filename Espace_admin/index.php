13951273c13a3e5826f4d0cf71189ea19324651d
abdelghani hamaz <abdelghani.hamaz.pro@gmail.com>
2024-05-01 20:09:13 +0200

    add deconnexion.php

--- Espace_admin/index.php
+++ Espace_admin/index.php
@@ -1,4 +1,21 @@
 <?php
 session_start();
-
+if(!$_SESSION['password']){
+    header('Location: connexion.php');
+}
 echo 'Welcome '.$_SESSION['username'].' in our website';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <a href="deconnexion.php">
+    <button>Deconnexion</button>
+    </a>
+</body>
+</html>
